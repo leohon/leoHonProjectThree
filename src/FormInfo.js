3603266db74f0b497505709aed1eef529ad67ece
leohon <leo.cl.hon@gmail.com>
2022-12-03 15:21:24 -0500

    created simple components

---
+++ src/FormInfo.js
@@ -0,0 +1,10 @@
+const Info = function() {
+  return (
+    <div className="info">
+      <h3>Form</h3>
+      <p>Form info</p>
+    </div>
+  )
+}
+
+export default Info;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+const Header = function() {
+  return (
+    <header>
+      {/* <nav>
+        <button>Log In</button>
+      </nav> */}
+
+      <h1>[TITLE]</h1>
+    </header>
+  )
+}
+
+export default Header;
