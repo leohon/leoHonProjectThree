c3eb31f7368de7045e638b7d5118ffe3cd26f6d1
leohon <leo.cl.hon@gmail.com>
2022-12-06 00:01:59 -0500

    reorganized code

--- src/Form.js
+++ src/Form.js
@@ -5,22 +5,44 @@ const Form = function(props) {
     <form >
         <div className="oneInput">
             <label htmlFor="newName">Name</label>
-            <input type="text" id="newName" onChange={props.nameInput} value={props.contactName} required/>
+            <input 
+                type="text" 
+                id="newName" 
+                onChange={props.nameInput} 
+                value={props.contactName} required
+                />
         </div>
 
         <div className="oneInput">
             <label htmlFor="newCo">Company</label>
-            <input type="text" id="newCo" onChange={props.companyInput} value={props.contactCompany} required/>
+            <input 
+                type="text" 
+                id="newCo" 
+                onChange={props.companyInput} 
+                value={props.contactCompany} 
+                required
+                />
         </div>
 
         <div className="oneInput">
             <label htmlFor="newEmail">Email</label>
-            <input type="email" id="newEmail" onChange={props.emailInput} value={props.contactEmail} required/>
+            <input 
+                type="email" 
+                id="newEmail" 
+                onChange={props.emailInput} 
+                value={props.contactEmail} 
+                required
+            />
         </div>
 
         <div className="oneInput">
             <label htmlFor="newPhone">Phone #</label>
-            <input type="tel" id="newPhone" onChange={props.phoneInput} value={props.contactPhone}/>
+            <input 
+                type="tel" 
+                id="newPhone" 
+                onChange={props.phoneInput} 
+                value={props.contactPhone}
+            />
         </div>
         
         <button onClick={props.handleSubmit}>Add</button>
