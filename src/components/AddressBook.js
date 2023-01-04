1aef11fdc31b41f0d951b6f32515543462ab07a8
leohon <leo.cl.hon@gmail.com>
2023-01-04 18:18:16 -0500

    removed phone # unless provided

--- src/components/AddressBook.js
+++ src/components/AddressBook.js
@@ -4,19 +4,35 @@ const AddressBook = function(props) {
     // Map through the database and display the new contact in a UL sticky note
     // Key pushed above to be used here
     props.contacts.map(function(contact) {
-      return (
-        <li key={contact.key} className="book">
-          <div className="buttonContainer">
-            <button onClick={function() {props.removeContact(contact.key)}}>X</button>
-          </div>
-          <div className="textContainer">
-            <p><i className="fa-solid fa-user"></i> - {contact.name}</p>
-            <p><i className="fa-solid fa-building"></i> - {contact.company}</p>
-            <p><i className="fa-solid fa-envelope"></i> - {contact.email}</p>
-            <p><i className="fa-solid fa-phone"></i> - {contact.phone}</p>
-          </div>
-        </li>
-      )
+      if (contact.phone === "") {
+        return (
+          <li key={contact.key} className="book">
+            <div className="buttonContainer">
+              <button onClick={function() {props.removeContact(contact.key)}}>X</button>
+            </div>
+            <div className="textContainer">
+              <p><i className="fa-solid fa-user"></i> - {contact.name}</p>
+              <p><i className="fa-solid fa-building"></i> - {contact.company}</p>
+              <p><i className="fa-solid fa-envelope"></i> - {contact.email}</p>
+            </div>
+          </li>
+        )
+      }
+      else {
+        return (
+          <li key={contact.key} className="book">
+            <div className="buttonContainer">
+              <button onClick={function() {props.removeContact(contact.key)}}>X</button>
+            </div>
+            <div className="textContainer">
+              <p><i className="fa-solid fa-user"></i> - {contact.name}</p>
+              <p><i className="fa-solid fa-building"></i> - {contact.company}</p>
+              <p><i className="fa-solid fa-envelope"></i> - {contact.email}</p>
+              <p><i className="fa-solid fa-phone"></i> - {contact.phone}</p>
+            </div>
+          </li>
+        )
+      }
     })
   )
 }
